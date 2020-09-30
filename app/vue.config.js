2cc62eb9d42c42343b0d13ac524ed5c26bf251d9
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-09-30 18:10:18 +0800

    Vue Einstellung setzen
    Vue Einstellung setzen

---
+++ app/vue.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/scss/abstracts/*.scss'),
+      ]
+    }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio/'
+    : '/'
+}
