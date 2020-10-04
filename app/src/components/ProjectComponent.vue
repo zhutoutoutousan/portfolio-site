8fd24bdb60426857c56dce7616f2ff1012c777b7
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-10-04 23:20:10 +0800

    Aktualisierung
    Aktualisierung

--- app/src/components/ProjectComponent.vue
+++ app/src/components/ProjectComponent.vue
@@ -10,12 +10,51 @@
               </div>
           </div>
       </div>
+
+      <div class="project__background">
+          <div class="project__model">
+              <buton class="project__close">
+                  <font-awesome-icon icon="times" />
+              </buton>
+
+              <div class="project__left">
+                  <img src="" alt="" class="project__img--modal">
+              </div>
+
+              <div class="project__right">
+                  <div class="project__label">Project</div>
+                  <h3 class="project__name"></h3>
+                  <ul class="project__technologies">
+                      <li class="project__technology"></li>
+                  </ul>
+                  <div class="project__label">About</div>
+                  <p class="project__summary">
+
+                  </p>
+                  <a href="" class="project__button project__button--modal">
+                      <font-awesome-icon icon="eye" /> Demo
+                  </a>
+                  <button class="project__button project__button--modal">
+                      <font-awesome-icon icon="code" />
+                  </button>
+                  <a href="" class="project__button project__button--modal">
+                      <font-awesome-icon icon="code" /> code
+                  </a>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+    props: ['project'],
+    data() {
+        return {
+            showProject: false
+        }
+    }
 }
 </script>
 
--- app/src/components/ProjectsComponent.vue
+++ app/src/components/ProjectsComponent.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="portfolio">
-      
+      <div class="row">
+        <project-component></project-component>
+      </div>
+      <div class="row">
+
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+import ProjectComponent from '@/components/ProjectComponent'
 
+export default {
+  components: { ProjectComponent },
+  data() {
+    return {
+      loading: true
+    }
+  }
 }
 </script>
 
